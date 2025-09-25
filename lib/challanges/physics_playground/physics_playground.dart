8d52635034b1f4e46c4ec29c8b9929051399a70a
Ahmed Abdelnasser <ahmedabdelnasser.dev@gmail.com>
2025-09-25 06:24:28 +0300

    done with the physics playground

--- lib/challanges/physics_playground/physics_playground.dart
+++ lib/challanges/physics_playground/physics_playground.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class PhysicsPlayground extends StatelessWidget {
+class PhysicsPlayground extends StatefulWidget {
   const PhysicsPlayground({super.key});
+
+  @override
+  State<PhysicsPlayground> createState() => _PhysicsPlaygroundState();
+}
+
+class _PhysicsPlaygroundState extends State<PhysicsPlayground> {
+  final List<Color> colors = [Colors.blue, Colors.red, Colors.green];
+  final double boxsideWidth = 4;
+  final size = 40.0;
+  late List<bool> isDropped;
+
+  @override
+  void initState() {
+    super.initState();
+    isDropped = List<bool>.filled(colors.length, false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +28,96 @@ class PhysicsPlayground extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         centerTitle: true,
         foregroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // filled color circles
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              for (var color in colors)
+                Draggable<Color>(
+                  data: color,
+                  feedback: Container(
+                     width: size * 1.4,
+                    height: size * 1.4,
+                    decoration: BoxDecoration(
+                      boxShadow:  [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      color: color,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+
+                  childWhenDragging: Container(
+                    width: size,
+                    height: size,
+                    decoration: BoxDecoration(
+                      color: color.withOpacity(0.3),
+                      shape: BoxShape.circle,
+                      
+                    ),
+                    
+                  ),
+
+                  child: Container(
+                    width: size,
+                    height: size,
+                    decoration: BoxDecoration(
+                      color: color,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+
+          // bordered squares
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(colors.length, (index) {
+              final color = colors[index];
+              return DragTarget<Color>(
+                builder: (context, candidateData, __) {
+
+                  return Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: color, width: boxsideWidth),
+                      borderRadius: BorderRadius.circular(8),
+                      color: isDropped[index] ? color : color.withOpacity(0.3),
+                    ),
+                    child: Center(
+                      child: isDropped[index]
+                          ? Icon(Icons.check, color: Colors.white, size: 32)
+                          : Icon( Icons.arrow_downward, color: candidateData.isNotEmpty ?  Colors.white : Colors.transparent, size: 32)
+                    )
+                  );
+                },
+
+                // when the draggable is hovering over the target
+                
+                
+                // When draggable is dropped correctly
+                onAccept: (receivedColor) {
+                  if (receivedColor == color) {
+                    setState(() {
+                      isDropped[index] = true;
+                    });
+                  }
+                },
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
--- lib/challanges/squensial_loading_dot/squensial_loading_dots.dart
+++ lib/challanges/squensial_loading_dot/squensial_loading_dots.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SquensialLoadingDots extends StatelessWidget {
+class SquensialLoadingDots extends StatefulWidget {
   const SquensialLoadingDots({super.key});
 
+  @override
+  State<SquensialLoadingDots> createState() => _SquensialLoadingDotsState();
+}
+
+class _SquensialLoadingDotsState extends State<SquensialLoadingDots> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +17,25 @@ class SquensialLoadingDots extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         centerTitle: true,
         foregroundColor: Colors.white,
+      ),
+      
+      body: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: List.generate(3, (index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
